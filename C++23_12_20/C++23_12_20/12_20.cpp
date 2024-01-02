2b3ee9d18ddd9b984b995c4f6015ed1d4e7a22cb
435191950@qq.com <435191950@qq.com>
2024-01-02 09:28:45 +0800

    复习

---
+++ C++23_12_20/C++23_12_20/12_20.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+
+
+//int main()
+//{
+//	cout << "hello world!" << endl;
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+
+//#define DAY 7
+//
+//int main()
+//{
+//	//int num = 10;
+//	//cout << "num = "<< num << endl;
+//
+//	cout << "一周一共有: " << DAY << " 天" << endl;
+//
+//	//const 修饰的变量
+//	const int month = 12;
+//	cout << "一年一共有: " << month << " 月" << endl;
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+
+int main()
+{
+	//int size = sizeof(int *);
+	//cout << size << endl;
+	//float f1 = 3.14f;
+	//double d1 = 2.14;
+
+	//cout << f1 << endl;
+	//cout << d1 << endl;
+
+	//cout << sizeof(float) << endl;
+	//cout << sizeof(double) << endl;
+
+
+	////科学计数法
+	//float f2 = 2e2;  //2* 10^2;
+	//cout << f2 << endl;
+	//float f3 = 3e-2;  //3*0.1^2
+	//cout << f3 << endl;
+
+	//char ch = 'a';
+	//cout << (int)ch << endl;
+
+	//char str[] = "hello world";
+
+	//cout << str << endl;
+
+	//string str2 = "FZU";
+	//cout << str2 << endl;
+
+
+	//bool flag = true;
+	//cout << flag << endl;
+
+	//flag = false;
+	//cout << flag << endl;
+
+	//cout << "size of bool = " << sizeof(bool) << endl;
+
+	////1.整型
+	//int a = 0;
+	//cout << "请给整型变量a赋值：" << endl;
+	//cin >> a;
+	//cout << "a = " << a << endl;
+
+	////2.浮点型
+	//float b = 0.0f;
+	//cout << "请给浮点型变量b赋值：" << endl;
+	//cin >> b;
+	//cout << "b = " << b << endl;
+
+	////3.字符型
+	//char c = 0;
+	//cout << "请给字符型变量c赋值：" << endl;
+	//cin >> c;
+	//cout << "c = " << c << endl;
+
+	////4.字符串型
+	//string d = "d";
+	//cout << "请给字符串型变量d赋值：" << endl;
+	//cin >> d;
+	//cout << "d = " << d << endl;
+
+	////5.布尔类型
+	//bool e = false;
+	//cout << "请给布尔型变量a赋值(0或1)：" << endl;
+	//cin >> e;
+	//cout << "e = " << e << endl;
+
+	//int a1 = 10;
+	//int b1 = 3;
+
+	//cout << a1 % b1 << endl;
+
+	//int a2 = 10;
+	//int b2 = 20;
+
+	//cout << a2 % b2 << endl;
+
+	//int a3 = 10;
+	//int b3 = 0;
+
+	//cout << a3 % b3 << endl;
+
+
+	////两个小数是不能做取模运算的
+	//double d1 = 3.14;
+	//double d2 = 1.1;
+	//cout << d1 % d2 << endl;
+
+
+ 
+	system("pause");
+	return 0;
+}
